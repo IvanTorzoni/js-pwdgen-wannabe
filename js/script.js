ffde0e694b9173744de3e49b6afb706ba259e953
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-03-22 13:58:12 +0100

    feat: creation of inputs and conversion of number into string

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+
+// input
+// Richiesta di inserimento nome all'utente tramite Prompt
+let userName = prompt("Inserisci il tuo nome"); //string
+// Log message alla console
+console.log(userName, typeof userName);
+
+// Richiesta di inserimento cognome all'utente tramite Prompt
+let userLastName = prompt("Inserisci il tuo cognome"); //string
+// Log message alla console
+console.log(userLastName, typeof userLastName);
+
+// Richiesta di inserimento colore preferito all'utente tramite Prompt
+let userColor = prompt("Inserisci il tuo colore preferito"); //string
+// Log message alla console
+console.log(userColor, typeof userColor);
+
+// Inserimento di un numero fisso
+let fixNum = 21 //number
+// Log message alla console
+console.log(fixNum, typeof fixNum);
+
+// converto il numero in striga
+fixNum = fixNum.toString(); //string
+// Log message alla console
+console.log(fixNum, typeof fixNum);
+
+
+// operation
+
+
+// output
